b3007dc2aad714530c843d09951c90351fe1ff4a
lxb1226 <1944303766@qq.com>
2021-09-22 23:12:37 +0800

    complete Q399

---
+++ build/src/CMakeFiles/question39.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question39.dir/question39.cpp.o"
+  "question39.pdb"
+  "question39"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/question39.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
