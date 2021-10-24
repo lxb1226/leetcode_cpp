37c9c0567feb0c58447630f72b8b7df3ccf2b9bb
lxb1226 <1944303766@qq.com>
2021-10-24 23:52:53 +0800

    update 20211024

--- src/question287.cpp
+++ src/question287.cpp
@@ -69,7 +69,38 @@ public:
 
         return ans;
     }
+    int findDuplicate1(vector<int> &nums)
+    {
+        int n = nums.size(), ans = 0;
+        int bit_max = 31;
+        while (!(n - 1) >> bit_max)
+        {
+            bit_max -= 1;
+        }
 
+        for (int bit = 0; bit <= bit_max; ++bit)
+        {
+            int x = 0, y = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (nums[i] & (1 << bit))
+                {
+                    x += 1;
+                }
+                if (i >= 1 && (i & (1 << bit)))
+                {
+                    y += 1;
+                }
+            }
+
+            if (x > y)
+            {
+                ans |= 1 << bit;
+            }
+        }
+
+        return ans;
+    }
     // 法三：快慢指针
     // TODO: 看不懂
     int findDuplicate2(vector<int> &nums)
--- src/question437.cpp
+++ src/question437.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -16,9 +17,65 @@ struct TreeNode
 class Solution
 {
 public:
+    // TODO:前缀和
+    unordered_map<long long, int> prefix;
+    int dfs(TreeNode *root, long long curr, int targetSum)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        int ret = 0;
+        curr += root->val;
+        if (prefix.count(curr - targetSum))
+        {
+            ret = prefix[curr - targetSum];
+        }
+
+        prefix[curr]++;
+        ret += dfs(root->left, curr, targetSum);
+        ret += dfs(root->right, curr, targetSum);
+        prefix[curr]--;
+        return ret;
+    }
+
     int pathSum(TreeNode *root, int targetSum)
     {
+        prefix[0] = 1;
+        return dfs(root, 0, targetSum);
+    }
+
+    // 法一：
+    int rootSum(TreeNode *root, int targetSum)
+    {
+        if (root == nullptr)
+        {
+            return 0;
         }
+        int ret = 0;
+        if (root->val == targetSum)
+        {
+            ret++;
+        }
+
+        ret += rootSum(root->left, targetSum - root->val);
+        ret += rootSum(root->right, targetSum - root->val);
+        return ret;
+    }
+
+    int pathSum(TreeNode *root, int targetSum)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        int ret = rootSum(root, targetSum);
+        ret += pathSum(root->left, targetSum);
+        ret += pathSum(root->right, targetSum);
+        return ret;
+    }
 };
 
 int main()
