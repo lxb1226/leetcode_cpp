29ef590c4328f64b99c0e8a932347a868300d299
lxb1226 <1944303766@qq.com>
2021-10-25 16:23:53 +0800

    update 20211025

--- src/question207.cpp
+++ src/question207.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -7,6 +8,7 @@ class Solution
 {
 private:
     vector<vector<int>> edges;
+    vector<int> indeg;
     vector<int> visited;
     bool valid = true;
 
@@ -54,6 +56,39 @@ public:
         }
 
         return valid;
+    }
+
+    // 法二：广度优先搜索 使用广度优先搜索做拓扑排序
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        edges.resize(numCourses);
+        indeg.resize(numCourses);
+        for(const auto& info : prerequisites){
+            edges[info[1]].push_back(info[0]);
+            ++indeg[info[0]];
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; ++i){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int visited = 0;
+        while(!q.empty()){
+            ++visited;
+            int u = q.front();
+            q.pop();
+            for(int v : edges[u]){
+                --indeg[v];
+                if(indeg[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+        
+        return visited == numCourses;
     }
 };
 
