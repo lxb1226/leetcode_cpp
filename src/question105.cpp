04a7836fcd6dcdf3372d0d8644dd51f85c188135
lxb1226 <1944303766@qq.com>
2021-10-31 22:55:14 +0800

    update 20211031

--- src/question105.cpp
+++ src/question105.cpp
@@ -13,13 +13,35 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        return restructTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size());
+    }
 
+    TreeNode *restructTree(vector<int> &preorder, vector<int> &inorder, int left_p, int right_p, int left_i, int right_i)
+    {
+        if (left_p > right_p)
+        {
+            return nullptr;
+        }
+        TreeNode *node = new TreeNode(preorder[left_p]);
+        int pos;
+        for (pos = left_i; pos <= right_i; ++pos)
+        {
+            if (inorder[pos] == preorder[left_p])
+            {
+                break;
+            }
+        }
+        node->left = restructTree(preorder, inorder, left_p + 1, pos - left_i + left_p, left_i, pos - 1);
+        node->right = restructTree(preorder, inorder, pos - left_i + left_p + 1, right_p, pos + 1, right_i);
+        return node;
     }
 };
 
-int main(){
-
+int main()
+{
 }
