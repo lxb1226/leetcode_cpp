875f0dbaf65bcd61536e74b9e1f315149556404f
lxb1226 <1944303766@qq.com>
2021-11-01 23:42:16 +0800

    update 20211101

--- src/question406.cpp
+++ src/question406.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,26 @@ public:
     // TODO:没有想法
     vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
     {
+        sort(people.begin(), people.end(), [](const vector<int>&u, const vector<int>&v){
+            return u[0] < v[0] || (u[0] == v[0] && u[1] > v[1]);
+        });
+
+        int n = people.size();
+        vector<vector<int>> ans(n);
+        for(const vector<int>& person : people){
+            int spaces = person[1] + 1;
+            for(int i = 0; i < n; ++i){
+                if(ans[i].empty()){
+                    --spaces;
+                    if(!spaces){
+                        ans[i] = person;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
 };
 
